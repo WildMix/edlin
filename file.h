6132454db5b7919aa5e8420c75f40f479d04a47a
doublegeek <46897339+WildMix@users.noreply.github.com>
2019-11-24 20:45:05 +0100

    Add files via upload

---
+++ file.h
@@ -0,0 +1,6 @@
+void InsertNewLines(int linestart, char *path, int times);
+int GetLine(int pos, char *path);
+void LoadText (char *path);
+int GetNumLines(char *path);
+void SaveText (char *path);
+
